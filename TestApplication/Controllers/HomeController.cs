f99dfd1c73120ca8c502eed23cbc73d9670c5616
Sohan Hasan <sohan.hasan9021@gmail.com>
2022-06-13 18:01:59 +0600

    update

---
+++ TestApplication/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult About()
+        {
+            string jsonData = "";
+            try
+            {
+                Stream req = Request.InputStream;
+                req.Seek(0, System.IO.SeekOrigin.Begin);
+                string json = new StreamReader(req).ReadToEnd();
+                List<Employee> employees = JsonConvert.DeserializeObject<List<Employee>>(json);
+                string ss = "";
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TestApplication/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult UpdateLastLocationCache()
+        {
+            //bool isCacheUpdated = false;
+            //try
+            //{
+            //    PointRepository _pointRepository = new PointRepository();
+            //    List<NewMovePointModel> newMovePointModels = _pointRepository.GetNewPointList();
+            //    isCacheUpdated = Mapping.HtTruckCollectionMapping.UpdateLastLocationCache("lastLocationList", newMovePointModels);
+            //}
+            //catch (Exception)
+            //{
+            //    throw;
+            //}
+            //return await Task.FromResult(isCacheUpdated);
+            return null;
+        }
+    }
+}
